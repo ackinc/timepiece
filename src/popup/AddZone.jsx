2513d94ba9ed2fa0cdbe873c6181be64c247c1ad
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-04-08 16:41:37 +0530

    No longer displaying a blank option at the top of zones list

--- src/popup/AddZone.jsx
+++ src/popup/AddZone.jsx
@@ -4,18 +4,15 @@ import PropTypes from "prop-types";
 import React, { useState } from "react";
 import { zonesArr } from "../common/zones";
 
-const blankOption = { name: "" };
-const options = [blankOption].concat(zonesArr);
-
 function AddZoneComponent({ onAdd }) {
   // If we don't control inputValue, the combobox will display entered text
   //   on field blur
   const [inputValue, setInputValue] = useState("");
-  const [value, setValue] = useState(blankOption);
+  const [value, setValue] = useState(null);
 
   return (
     <Autocomplete
-      options={options}
+      options={zonesArr}
       getOptionLabel={({ name }) => name}
       renderInput={(params) => (
         <TextField {...params} label="Add Timezone" variant="outlined" />
@@ -35,7 +32,7 @@ function AddZoneComponent({ onAdd }) {
   async function handleChange(_, { name }) {
     if (name) await onAdd(name);
     setInputValue("");
-    setValue(blankOption);
+    setValue(null);
   }
 }
 
